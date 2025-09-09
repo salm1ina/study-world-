c0c156f5709a597bf8a3726695eb0f3692340071
iLa <nabilaakbars1@gmail.com>
2025-09-09 19:52:43 +0800

    mencoba

--- learnpemvis.py
+++ learnpemvis.py
@@ -1,1 +1,18 @@
 import tkinter as tk
+
+window= tk.Tk()
+lebar =400
+tinggi =600
+x =500
+y =100
+
+window.title("Coba gais")
+
+screenwidth = window.winfo_screenwidth()
+screenheight = window.winfo_screenheight()
+newx = int((screenwidth/2) - (lebar/2))
+newy = int((screenheight/2) - (tinggi/2))
+
+window.geometry(f"{lebar}x{tinggi}+{newx}+{newy}")
+
+window.mainloop()
